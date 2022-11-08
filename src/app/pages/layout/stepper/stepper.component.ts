9cb8a68e9c850506c145f9e5656da5a8b4da06af
juny524 <juny5241@gmail.com>
2022-11-09 06:50:58 +0900

    Googleの認証をした でもそのレスポンスを利用できない

--- src/app/pages/layout/stepper/stepper.component.ts
+++ src/app/pages/layout/stepper/stepper.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import {
   NbButtonModule,
   NbCardModule,
@@ -85,11 +86,28 @@ export class StepperComponent implements OnInit {
   }
 
   get_test_tournament(){
-    const result = this.datamanage.getDataList();
-    result.then(e=>{
-      console.log(e);
-      console.log(e[0]);
-    });
+
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // ...
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+
   }
 
 
---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { GoogleAuthProvider } from "firebase/auth";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+}
